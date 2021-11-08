5d115b37d6654a675da759dde8743a7697347604
Gui37 <guifty32@gmail.com>
2021-11-08 20:59:55 +0200

    Correção de rotas

--- app.js
+++ app.js
@@ -89,7 +89,31 @@ app.post('/blogs',(req, res) =>{
     })
 })
 
-app.get('/create',(req, res) =>{
+app.get('/blogs/:id',(req,res) => {
+    const id= req.params.id;
+    Blog.findById(id)
+    .then (result =>{
+        res.render('details', {blog: result, title: 'Blog Details'});
+    })
+    .catch(err =>{
+        console.log(err)
+    })
+})
+
+app.delete('/blogs/:id',(req,res) => {
+    const id= req.params.id;
+    
+    Blog.findByIdAndDelete(id)
+     .then (result => {
+        res.json({ redirect: '/blogs'})
+    })
+    .catch(err => {
+        console.log(err)
+    })
+})
+
+
+app.get('/blogs/create',(req, res) =>{
     res.render('create', { title:'Criar um novo blog'});
 })
 
